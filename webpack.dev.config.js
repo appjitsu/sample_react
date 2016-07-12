d64368c2d106b70cf463d718a1b0cc59584b9797
Jason Cochran <jlcochran2013@gmail.com>
2016-07-12 04:59:56 -0500

    production build;

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-hot-middleware/client',
+    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
@@ -13,21 +13,19 @@ module.exports = {
     publicPath: '/public/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        loader: 'babel',
+        test: /\.jsx$/,
+        loaders: ['react-hot', 'babel'],
         exclude: /node_modules/
       },
       {
-        test: /\.jsx?$/,
-        loader: 'babel',
-        include: path.join(__dirname, 'src/components'),
-        exclude: /node_modules/
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       },
       {
         test: /\.json$/,
