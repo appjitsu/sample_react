435b8d594987207fba641de3c11427bac834f2c3
Jason Cochran <jlcochran2013@gmail.com>
2016-07-12 03:39:31 -0500

    verbiage change;

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,7 @@ export default class About extends React.Component {
     return (
       <div className="col-lg-12 text-center">
         <h1>About</h1>
-        <p className="lead">This is a test app to demonstrate that I have the
-        skills necessary to do React web development.</p>
+        <p className="lead">This is a sample web app tech demo for React.</p>
       </div>
     );
   }
